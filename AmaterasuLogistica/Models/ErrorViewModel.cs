d50b8da2a432cfbc964e3714ee04b74df33fac92
David williams pyrrho de lima <78514902+davidmidoria@users.noreply.github.com>
2023-11-09 12:27:19 -0300

    Adicionar arquivos de projeto.

---
+++ AmaterasuLogistica/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace AmaterasuLogistica.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
